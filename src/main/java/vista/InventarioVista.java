e6ebc9554d4bfbf5f763ff59322e78488d303db7
David Schneider Bustamante <dbustamante@estudiante.uniajc.edu.co>
2025-11-17 21:01:57 -0500

    feat(vista): agregar constructor e inicializar Scanner

--- src/main/java/vista/InventarioVista.java
+++ src/main/java/vista/InventarioVista.java
@@ -1,5 +1,13 @@
 package vista;
 
+import java.util.Scanner;
+
 public class InventarioVista {
+
+    private final Scanner scanner;
+
+    public InventarioVista() {
+        this.scanner = new Scanner(System.in);
+    }
 
 }
